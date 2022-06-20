f837dcb0b6eae8d58f87decaf7841fa85f2d1a5b
PureColo <rainer@purecolo.ca>
2022-06-19 23:35:28 -0400

    V0.5.0

--- cypress.config.ts
+++ cypress.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     e2e: {
         baseUrl: 'http://localhost:3000',
         supportFile: "tests/cypress/support/e2e.ts",
-        specPattern: "imports/**/tests/cypress/*.cy.js",
+        specPattern: "imports/**/tests/e2e/*.cy.js",
 
         setupNodeEvents(on, config) {
           // implement node event listeners here
