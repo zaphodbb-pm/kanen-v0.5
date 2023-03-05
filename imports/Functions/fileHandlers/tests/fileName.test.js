648a6c805e35aa62cd8c32a054dfcb4e3a26cd47
ZaphodBB <eigentum@me.com>
2023-03-05 15:59:13 -0500

    V0.5.0

--- imports/Functions/fileHandlers/tests/fileName.test.js
+++ imports/Functions/fileHandlers/tests/fileName.test.js
@@ -10,12 +10,13 @@ import {fileName} from "../fileName";
 describe("function fileName", function () {
   data.forEach( (test, idx) => {
 
-    it(`test ${idx + 1}: ${test.name} + ${test.ext}`, function(){
+    it(`test ${idx + 1}: ${test.name} + date + ${test.ext}`, function(){
       const result = fileName(test.name, test.ext);
 
-      // added date part: _2023-02-11#13_13 is 17 characters long
+      // added date part: _2023-02-11#13_13 is 16 or 17 characters long
       // must also account for added "." for the extension part
-      const dateLength = result.length - test.name.length - test.ext.length - 1 === 17;
+      const fileLength = result.length - test.name.length - test.ext.length - 1;
+      const dateLength =  fileLength === 16 || fileLength === 17;
 
       const testResult1 = result.startsWith(test.name);
       const testResult2 = result.includes("#") && dateLength;
