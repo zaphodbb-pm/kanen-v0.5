9a0fed4fbbe09b3b11eb3cbcef922458105c26f0
ZaphodBB <eigentum@me.com>
2023-03-24 18:18:03 -0400

    V0.5.0

--- imports/server/main_server.app-test.js
+++ imports/server/main_server.app-test.js
@@ -1,3 +1,15 @@
 console.log("in main server app-test");
 
 import './tests/server.test';
+
+/** methods test **/
+
+/*
+    documentation.test
+    exportImport.test
+    readDocs.test
+    storeDoc.test
+    system.test
+    userMgmt.test
+
+ */
